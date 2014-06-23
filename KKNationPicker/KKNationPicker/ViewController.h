7bdd6801be1d8ea2cf1082a988497bd22b941ce3
dthanhtl <orb@ThanhTranORB.local>
2014-06-23 18:38:37 +0700

    up
    up

---
+++ KKNationPicker/KKNationPicker/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  KKNationPicker
+//
+//  Created by Thanh Tran on 6/23/14.
+//  Copyright (c) 2014 ThanhTran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ KKNationPicker/KKNationPickerTests/KKNationPickerTests.m
@@ -0,0 +1,34 @@
+//
+//  KKNationPickerTests.m
+//  KKNationPickerTests
+//
+//  Created by Thanh Tran on 6/23/14.
+//  Copyright (c) 2014 ThanhTran. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface KKNationPickerTests : XCTestCase
+
+@end
+
+@implementation KKNationPickerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
